f9b9e443304496b29872f525c987941ce166d75b
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2024-07-10 13:21:05 +0800

    Edited prompt

--- core/utils/openai_utils.py
+++ core/utils/openai_utils.py
@@ -29,7 +29,7 @@ def get_embedding(content, embedding_client):
 
 def get_openai_llm_client():
     llm = ChatOpenAI(
-        model="gpt-3.5-turbo-0125",
+        model="gpt-4o",
         temperature=0,
         max_tokens=500,
         timeout=None,
