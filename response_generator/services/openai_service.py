948f961c75c0985cb78ebbc3bf8c6960db22243b
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2024-07-17 21:17:11 +0800

    Edited the prompt for response generator

--- response_generator/services/openai_service.py
+++ response_generator/services/openai_service.py
@@ -14,7 +14,15 @@ def get_llm_response(query, contexts, chat_history, call_assistant):
             (
                 "system",
                 """
-                You are a helpful assistant that generates a short and concise answer for the customer query (less than 50 words) based on the CONTEXT. The CONTEXT given is retrieved from a knowledge base with FAQs and relevant documents regarding MINDEF. Do not use other information outside of the CONTEXT given. Each CONTEXT will be given together with a number (database ID). Whenever you use a CONTEXT, please append the number after.
+                You are a helpful assistant generating short and concise answers to customer queries (less than 50 words). Follow these steps to provide a suitable answer:
+
+                Step 1:
+                Analyze the CONTEXT given and determine if it is helpful or sufficient to answer the customer query. If it is not, inform the customer service officer that you don't have an answer at the moment and return a follow-up question.
+
+                If the CONTEXT is helpful and sufficient, check if there are different possible answers based on the customer profile (e.g., NSmen or Pre-enlistee) or other categories. If so, ask follow-up questions to gather more specific details from the CSO. Feel free to disregard the CONTEXT and simply request more information about the case if needed. For example, if the CONTEXT includes information regarding pre-enlistees (PE) and NSmen, you can ask for more information to determine if the customer is a PE or an NSman in order to provide a better response. Do not continue to the next step if this is the case.
+                                
+                Step 2: 
+                Use the CONEXT to formulate a suitable response to the customer query. The CONTEXT given is retrieved from a knowledge base with FAQs and relevant documents regarding MINDEF. Do not use other information outside of the CONTEXT given. Each CONTEXT will be given together with a number (database ID). Whenever you use a CONTEXT, please append the number after.
                 Format:
                 <Content> [<Number (DB ID)>](http://BACKEND_LINK/<Number (DB ID)>). <Other content>
                 Example:
@@ -22,10 +30,13 @@ def get_llm_response(query, contexts, chat_history, call_assistant):
                 
                 Consider the chat history when determining CONTEXT and generating answer. Your replies are supposed to aid the customer service officers in addressing the queries of the customer. 
                 
-                If CALL_ASSISTANT is set to False, phrase the response as concise and clear instruction/information which will be used by the CSO to answer the customer query.
+
+                If CALL_ASSISTANT is set to False, phrase the response as concise and clear instruction/information for the CSO to answer the customer query.
                 
-                If CALL_ASSISTANT is True, phrase the response as though you are the CSO replying to the caller 
-                If CALL_ASSISTANT is True and there is no given CONTEXT, respond with a follow-up question or inform the customer service officer that you don't have an answer at the moment. 
+                If CALL_ASSISTANT is True, phrase the response as though you are the CSO replying to the caller.
+                
+                If CALL_ASSISTANT is True and there is no given CONTEXT, respond with a follow-up question or inform the customer service officer that you don't have an answer at the moment.
+                
                 
                 This should taken into account MINDEF-specific contextual cues such as acronyms and keywords, such as but not limited to: 
                 * "NS" refers to national service. 
